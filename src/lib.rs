3cd97165bce97d5f10302fa017b4d2fbfc112d95
Abram Clements <abram.j.clements@gmail.com>
2022-12-03 15:51:24 -0700

    add ability to run multiple years of solutions

--- src/lib.rs
+++ src/lib.rs
@@ -18,17 +18,18 @@ impl AdventYear for Year {
     }
 
     fn solve(&self, day: usize) {
+        // call latest if day is set to 0
         if day == 0 {
             return self.solve_latest();
         }
 
-        assert!(day > 0 && day < 26, "day must be 1-25");
+        assert!(day > 0 && day < self.days.len(), "day {} is unimplemented. days {}-{} are implemented for selected year. 0 for latest", day, 1, self.days.len());
 
         if day >= self.days.len() {
             println!("unimplemented");
         }
 
-        self.days[day]()
+        self.days[day - 1]()
     }
 
     fn solve_latest(&self) {
@@ -71,8 +72,13 @@ impl AdventManager {
 
 
     pub fn solve_day(&self, year: usize, day: usize) {
+        // call latest year if year is set to 0
+        if year == 0 {
+            return self.years.last().unwrap().solve(day);
+        }
+
         assert!(year >= self.first_year && year < (self.first_year + self.years.len()),
-        "{} has no available solutions. Solutions are available for the years {}-{}", year, self.first_year, self.first_year + self.years.len() - 1);
+        "{} has no available solutions. Solutions are available for the years {}-{}. 0 for latest", year, self.first_year, self.first_year + self.years.len() - 1);
 
         self.years[year - self.first_year].solve(day)
     }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
-use advent_of_code::day1;
+use std::env;
 
 fn main() {
-    day1::solve();
+    let args: Vec<String> = env::args().collect();
+
+    let mut year = 0;
+    let mut day = 0;
+
+    // parse year and day if available
+    if args.len() >= 2 {
+        year = args[1].parse().expect(&format!("{} is not a valid year", args[1]));
+    }
+    if args.len() >= 3 {
+        day = args[2].parse().expect(&format!("{} is not a valid day", args[2]));
+    }
+
+    let advent_manager = advent_of_code::init();
+
+    // run solution for a day
+    advent_manager.solve_day(year, day);
 }
