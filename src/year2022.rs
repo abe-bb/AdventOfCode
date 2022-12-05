a9ae70bf78ca7e03ee9596839e8997a3c7d80939
Abram Clements <abram.j.clements@gmail.com>
2022-12-04 19:38:01 -0700

    add 2022 day 3 part 1 and 2 solutions

--- src/year2022.rs
+++ src/year2022.rs
@@ -1,10 +1,11 @@
 use std::fs::File;
 use std::io::{ self, BufRead, BufReader };
+use std::collections::HashSet;
 use crate::{ AdventYear, Year };
 
 pub fn init() -> Box<dyn AdventYear> {
     let days: Vec<Box<dyn Fn()>> = vec![
-        Box::new(day1), Box::new(day2)
+        Box::new(day1), Box::new(day2), Box::new(day3)
     ];
 
     Box::new(Year {
@@ -13,6 +14,108 @@ pub fn init() -> Box<dyn AdventYear> {
     })
 }
 
+fn day3() {
+    let reader = BufReader::new(File::open("./inputs/2022/day3/input").expect("can't read 2022 day3 input"));
+    let mut rucksacks: Vec<Rucksack> = reader.lines()
+        .map(|line| { Rucksack::new(line.expect("io error on line")) })
+        .collect();
+
+    let rucksack_priority_sum: usize = rucksacks.iter_mut()
+        .map(|rucksack| { rucksack.common_priority() })
+        .sum();
+    println!("Sum of priorities: {}", rucksack_priority_sum);
+
+
+    // check that there are only complete groups
+    let group_size = 3;
+    assert!(rucksacks.len() % group_size == 0, "Incomplete groups found in input");
+
+    let mut badges: Vec<char> = vec![];
+    for i in (0..rucksacks.len()).step_by(group_size) {
+        let group_intersection = &(&rucksacks[i].item_set & &rucksacks[i + 1].item_set) & &rucksacks[i + 2].item_set;
+        assert!(group_intersection.len() == 1, "multiple badge options found for a single group");
+        badges.push(group_intersection.into_iter().next().unwrap());
+    }
+
+    let group_priority_sum: usize = badges.iter()
+        .map(|badge| Rucksack::compute_item_priority(*badge))
+        .sum();
+
+    println!("Sum of badge priorities: {}", group_priority_sum);
+}
+
+struct Rucksack {
+    total: Box<String>,
+    pub item_set: HashSet<char>,
+    midpoint: usize,
+    common_item: Option<char>,
+    common_priority: Option<usize>,
+}
+
+impl Rucksack {
+    pub fn new(inventory: String) -> Rucksack {
+        // check function invariants
+        assert!(inventory.len() % 2 == 0);
+        assert!(inventory.is_ascii());
+        assert!(inventory.chars().all(char::is_alphabetic));
+
+        let midpoint = inventory.len() / 2;
+
+        let item_set = HashSet::from_iter(inventory.chars());
+
+        Rucksack {
+            total: Box::new(inventory),
+            item_set,
+            midpoint,
+            common_item: None,
+            common_priority: None,
+        }
+    }
+
+    pub fn common_item(&mut self) -> char {
+        let compartment1 = &self.total[..self.midpoint];
+        let compartment2 = &self.total[self.midpoint..];
+
+        let mut common_item = None;
+        for item in compartment1.chars() {
+            if compartment2.contains(item) {
+                match common_item {
+                    Some(x) => assert!(x == item, "multiple matching items in Rucksack"),
+                    None => common_item = Some(item),
+                }
+            }
+        }
+
+        if let None = common_item { panic!("Unable to find matching item in Rucksack") }
+
+        self.common_item = common_item;
+        common_item.unwrap()
+    }
+
+    pub fn common_priority(&mut self) -> usize {
+        let common_item = match self.common_item {
+            None => self.common_item(),
+            Some(item) => item,
+        };
+
+        self.common_priority = Some(Rucksack::compute_item_priority(common_item));
+        self.common_priority.unwrap()
+    }
+
+    fn compute_item_priority(item: char) -> usize {
+        let upper_case_offset = 38;
+        let lower_case_offset = 96;
+
+        let mut buff = [0; 4];
+        item.encode_utf8(&mut buff);
+        if item.is_uppercase() {
+            usize::from(buff[0]) - upper_case_offset
+        }
+        else {
+            usize::from(buff[0]) - lower_case_offset
+        }
+    }
+}
 
 fn day2() {
     let reader = BufReader::new(File::open("./inputs/2022/day2/input").expect("unable to read input file for 2022 day2"));
